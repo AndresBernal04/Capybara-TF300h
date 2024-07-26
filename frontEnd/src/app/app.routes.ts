8647877d29569ce7680b60874f42a8988f65f179
AndresBernal04 <andresbernal.afbr@gmail.com>
2024-07-26 10:41:18 -0500

    [update]

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GaleryComponent } from './components/galery/galery.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [ 
+    {path:'home', title: 'Home', component: HomeComponent},
+     // Nos sirve para enrutar
+    {path: 'contact', title: 'Contact', component: ContactComponent},
+    {path: 'galery', title: 'Galery', component: GaleryComponent},
+    {path: '**', title: '404', component: PageNotFoundComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
+
+
